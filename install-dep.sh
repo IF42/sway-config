dd5250490ff676a4df7fc267ea7d733590e97b6a
Petr Horáček <phoracek.edit@gmail.com>
2023-07-27 13:59:34 +0200

    added support for screenshots

--- install-dep.sh
+++ install-dep.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-dnf install sway waybar swaybg swayidle swaylock bemenu alacritty light alsa-utils fira-code-fonts fontawesome-fonts powerline-fonts
+dnf install sway waybar swaybg swayidle swaylock bemenu alacritty light alsa-utils fira-code-fonts fontawesome-fonts powerline-fonts grim grimshot

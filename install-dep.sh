cb88744c133a428c929ead7d4f2f840f9d28711f
Petr Horáček <phoracek.edit@gmail.com>
2025-04-18 15:06:34 +0200

    dependency update

--- install-dep.sh
+++ install-dep.sh
@@ -2,4 +2,4 @@
 
 #dnf install sway waybar swaybg swayidle swaylock bemenu alacritty light alsa-utils fira-code-fonts fontawesome-fonts powerline-fonts grim grimshot
 
-pacman -S sway waybar swaybg swayidle swaylock bemenu alacritty alsa-utils ttf-fira-code powerline-fonts ttf-font-awesome grim xorg-xwayland
+pacman -S sway waybar swaybg swayidle swaylock bemenu alacritty alsa-utils ttf-fira-code powerline-fonts ttf-font-awesome slurp grim xorg-xwayland udisks2
--- sway_bar.sh
+++ sway_bar.sh
@@ -27,7 +27,8 @@ loadavg_5min=$(cat /proc/loadavg | awk -F ' ' '{print $2}')
 network=$(ip route get 1.1.1.1 | grep -Po '(?<=dev\s)\w+' | cut -f1 -d ' ')
 # interface_easyname grabs the "old" interface name before systemd renamed it
 interface_easyname=$(dmesg | grep $network | grep renamed | awk 'NF>1{print $NF}')
-ping=$(ping -c 1 www.google.cz | tail -1| awk '{print $4}' | cut -d '/' -f 2 | cut -d '.' -f 1)
+gateway=$(ip route | awk '/default/ {print $3}')
+ping=$(ping -c 1 $gateway | tail -1| awk '{print $4}' | cut -d '/' -f 2 | cut -d '.' -f 1)
 
 
 
